c9052f0307b5072002e8c570795fcde028a2565f
massimo <massipetra@hotmail.it>
2023-12-07 16:33:53 +0100

    feat: personal paginator added

--- src/app/features/mycology/mycology-mushroom-list/mycology-mushroom-list.component.ts
+++ src/app/features/mycology/mycology-mushroom-list/mycology-mushroom-list.component.ts
@@ -1,15 +1,25 @@
-import { Component, Input, OnInit, OnChanges, SimpleChanges, OnDestroy } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  OnChanges,
+  SimpleChanges,
+  OnDestroy,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Mushroom } from '../../models/mushroom.models';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterLink, Router, RouterModule } from '@angular/router';
 import { Store } from '@ngrx/store';
-import *as MushroomsActions from '../../mycology-state/mycology.actions';
+import * as MushroomsActions from '../../mycology-state/mycology.actions';
 import { MycologyState } from '../../models/mycology-state.models';
-import { selectMushrooms, selectXtotalcount, selectPageIndex } from '../../mycology-state/mycology.selectors';
+import {
+  selectMushrooms,
+  selectXtotalcount,
+  selectPageIndex,
+} from '../../mycology-state/mycology.selectors';
 import { Observable, Subscription } from 'rxjs';
-import { MatPaginatorModule, PageEvent } from '@angular/material/paginator'
-
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-mycology-mushroom-list',
@@ -18,48 +28,75 @@ import { MatPaginatorModule, PageEvent } from '@angular/material/paginator'
   templateUrl: './mycology-mushroom-list.component.html',
   styleUrl: './mycology-mushroom-list.component.scss',
 })
-export class MycologyMushroomListComponent implements OnInit, OnDestroy{
+export class MycologyMushroomListComponent implements OnInit, OnDestroy {
   constructor(private router: Router, private store: Store<MycologyState>) {}
 
-mushrooms$ = this.store.select(selectMushrooms);
-@Input() xtotalcount$ ! : Observable<number>
-pageIndex$ = this.store.select(selectPageIndex)
+  mushrooms$ = this.store.select(selectMushrooms);
+  @Input() xtotalcount$!: Observable<number>;
+  pageIndex$ = this.store.select(selectPageIndex);
 
-pageIndex: number = 1
-xtotalcount: number = 0
-subs = new Subscription()
-
-
-handlePageEvent(event: PageEvent) {
-  this.pageIndex = event.pageIndex;
-  this.xtotalcount = event.length;
-  this.store.dispatch((MushroomsActions.loadMushrooms({pageIndex: this.pageIndex})))
-}
-  ngOnInit(): void {
-    this.store.dispatch(MushroomsActions.loadMushrooms({pageIndex: this.pageIndex}));
-    this.xtotalcount$ = this.store.select(selectXtotalcount);
-    console.log('initial: x total count: ', this.xtotalcount)
-    this.subs = this.xtotalcount$.subscribe((xtotal)=> {
-      this.xtotalcount = xtotal
-      console.log('new x total count: ', this.xtotalcount)
-    })
-    
+  totalPages!: number;
+  pageIndex: number = 1;
+  xtotalcount: number = 0;
+  pageSize!: number
+  subs = new Subscription();
+  isPrevDisabled!: boolean;
+  isNextDisabled!: boolean;
+  onNextPage() {
+    this.pageIndex = this.pageIndex + 1;
+    this.totalPages = Math.ceil(this.xtotalcount / this.pageSize);
+    if (this.pageIndex > 1) {
+      this.isPrevDisabled = false;
+    }
+    if (this.pageIndex === this.totalPages) {
+      this.isNextDisabled = true;
+    }
+    this.store.dispatch(
+      MushroomsActions.loadMushrooms({ pageIndex: this.pageIndex })
+    );
   }
-  
+  onPrevPage() {
+    this.pageIndex = this.pageIndex - 1;
+    if (this.pageIndex === 1) {
+      this.isPrevDisabled = true;
+      this.isNextDisabled = false;
+    }
 
+    this.store.dispatch(
+      MushroomsActions.loadMushrooms({ pageIndex: this.pageIndex })
+    );
+  }
+
+  ngOnInit(): void {
+    this.isPrevDisabled = true;
+    this.store.dispatch(
+      MushroomsActions.loadMushrooms({ pageIndex: this.pageIndex })
+    );
+    this.xtotalcount$ = this.store.select(selectXtotalcount);
+    console.log('initial: x total count: ', this.xtotalcount);
+   
+    this.subs.add(
+      this.xtotalcount$.subscribe((xtotal) => {
+        this.xtotalcount = xtotal;
+        console.log('new x total count: ', this.xtotalcount);
+      })
+    )
+    this.subs.add(
+      this.mushrooms$.subscribe((mushrooms)=> {
+        this.pageSize = mushrooms.length
+      })
+    )
+  }
 
   onMushroom(id: number | undefined) {
-    this.router.navigate([{outlets: {container: ['mushroom', id]}} ]);
-
+    this.router.navigate([{ outlets: { container: ['mushroom', id] } }]);
   }
 
   toNewMushroom() {
-    this.router.navigate([{outlets: {container: ['create-mushroom']}}])
+    this.router.navigate([{ outlets: { container: ['create-mushroom'] } }]);
   }
 
   ngOnDestroy(): void {
-   
-    this.subs.unsubscribe()
+    this.subs.unsubscribe();
   }
 }
-
