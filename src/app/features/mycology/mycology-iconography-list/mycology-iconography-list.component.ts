f2907ddbe27530b00e586c0b39b62bfae6ec2b6c
Massimo <massipetra@hotmail.it>
2023-12-23 11:38:26 +0100

    feat: bind mushroomID and iconographyID

--- src/app/features/mycology/mycology-iconography-list/mycology-iconography-list.component.ts
+++ src/app/features/mycology/mycology-iconography-list/mycology-iconography-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,5 +9,5 @@ import { CommonModule } from '@angular/common';
   styleUrl: './mycology-iconography-list.component.scss'
 })
 export class MycologyIconographyListComponent {
-
+@Input()  iconographyID!: number
 }
