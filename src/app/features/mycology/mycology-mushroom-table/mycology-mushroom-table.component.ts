955068a1ac093e4583b06e1b1c673c6f3bee9a5a
massimo <massipetra@hotmail.it>
2024-01-04 20:35:13 +0100

    feat:  base table added

--- src/app/features/mycology/mycology-mushroom-table/mycology-mushroom-table.component.ts
+++ src/app/features/mycology/mycology-mushroom-table/mycology-mushroom-table.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, OnChanges, SimpleChanges, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterLink, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { MycologyState } from '../../models/mycology-state.models';
 import {MatTableModule, MatTable} from '@angular/material/table'
+import { MatButtonModule } from '@angular/material/button';
 import { selectPageIndex } from '../../mycology-state/mycology.selectors';
 
 import { FormsModule } from '@angular/forms';
@@ -13,21 +14,25 @@ import { Mushroom } from '../../models/mushroom.models';
 @Component({
   selector: 'app-mycology-mushroom-table',
   standalone: true,
-  imports: [CommonModule, MatSidenavModule, MatTableModule, RouterLink, FormsModule],
+  imports: [CommonModule, MatSidenavModule, MatTableModule, RouterLink, FormsModule, MatButtonModule],
   templateUrl: './mycology-mushroom-table.component.html',
   styleUrl: './mycology-mushroom-table.component.scss',
 })
-export class MycologyMushroomTableComponent {
+export class MycologyMushroomTableComponent   {
   constructor(private router: Router, private store: Store<MycologyState>) {}
+  @ViewChild(MatTable) table! : MatTable<Mushroom>
+
+  columsToDisplay = ['species', 'gender', 'family', 'order', 'AA']
 
   @Input() mushrooms: Mushroom[] = [];
 
   //pageIndex$ = this.store.select(selectPageIndex);
   
   @Input() page!: number;
-  @Input() xtotalcount!: number;
 
   showFirstLastButtons: boolean = true;
+
+
 
   onMushroom(id: number) {
     this.router.navigate(['mushroom', this.page, id]);
